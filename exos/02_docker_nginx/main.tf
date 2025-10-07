50fde87472fe7dcd2a269f137b4db6de14305344
Adamsko3 <adamasanogo@monemail.com>
2025-10-07 17:36:17 +0200

    Exercice All projets Terraform

--- exos/02_docker_nginx/main.tf
+++ exos/02_docker_nginx/main.tf
@@ -2,22 +2,26 @@ terraform {
   required_providers {
     docker = {
       source  = "kreuzwerker/docker"
-      version = "~> 3.0"
+      version = "~> 3.0.1"
     }
   }
 }
 
 provider "docker" {}
 
+# Image Docker Nginx
 resource "docker_image" "nginx" {
-  name = "nginx:latest"
+  name         = "nginx:latest"
+  keep_locally = false
 }
 
+# Conteneur Nginx
 resource "docker_container" "nginx" {
-  image = docker_image.nginx.name
-  name  = "terraform-nginx"
+  name  = "nginx_server"
+  image = docker_image.nginx.image_id
+
   ports {
     internal = 80
-    external = 8080
+    external = var.external_port
   }
 }
